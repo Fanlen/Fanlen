22c372b1d467588606c68b26184f55ff1d66f5ce
FanLen <70324486@qq.com>
2017-02-04 03:00:40 +0000

    修改

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,7 +8,7 @@
       你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的示例项目主页。
     </p>
     <p>
-      一切，将从这里开始。
+      一切，将从这里开始。Common go!
     </p>
     <p>
       <a class="btn btn-lg btn-success" href="{{ route('singup') }}" role="button">现在注册</a>
