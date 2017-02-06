2947baf5883e21d7df42dd0001b4be5318788f05
FanLen <70324486@qq.com>
2017-02-06 09:58:21 +0000

    用户信息页面完善

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,10 +28,28 @@ class UsersController extends Controller
 
     }
 
-    public function gravatar($size = '100')
+    //处理表单提交后的数据
+    public function store(Request $request)
     {
-        $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'confirmed'
+        ]);
+
+        $user = User::create([
+
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
     }
 
 }
