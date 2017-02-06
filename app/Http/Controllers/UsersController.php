eb4f328de46ab0f394035192007d2228aea23ecd
FanLen <70324486@qq.com>
2017-02-06 02:48:35 +0000

    0206

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
+    //创建用户的页面
     public function create()
     {
         return view('users.create');
     }
 
+    //显示所有用户列表的页面
+    public function index()
+    {
+        return view('users.create');
+    }
 
-
-
+    //响应资源路由显示用户个人信息的页面
     public function show($id)
     {
 
@@ -22,4 +27,11 @@ class UsersController extends Controller
       return view('users.show', compact('user'));
 
     }
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
 }
